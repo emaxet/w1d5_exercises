1a645ac3ba4f385e150784fe953f422e3c9a5102
Erik Tammpere <etammpere@gmail.com>
2017-11-03 21:42:10 +0000

    fixed palindromes.js isPalindrome function so it fulfills testing requirements

---
+++ web-js-palindromes/lib/palindromes.js
@@ -0,0 +1,7 @@
+function isPalindrome(s) {
+  var stringNoSpaces = s.split(" ").join("");
+  var stringReverse = stringNoSpaces.split("").reverse().join("");
+  return stringNoSpaces == stringReverse;
+}
+
+module.exports = isPalindrome;
