9b89e41480c6a8ac573191a3c5cd837951027b87
Erik Tammpere <etammpere@gmail.com>
2017-11-04 07:33:53 +0000

    Luhn test

--- modulus10/lib/check.js
+++ modulus10/lib/check.js
@@ -1,3 +1,26 @@
 function check(num) {
-
+  var reversed = num.toString().split('').reverse();
+  var oddSum = 0;
+  var evenSum = 0;
+  for (var i = 1; i <= reversed.length; i++) {
+    if (i % 2 !== 0) {
+      oddSum += Number(reversed[i - 1]);
+    } else if (i % 2 === 0) {
+      reversed[i - 1] = reversed[i - 1] * 2;
+    }
+  }
+  for (var i = 1; i < reversed.length; i += 2) {
+    if(reversed[i] > 9) {
+      reversed[i] -= 9;
+    }
+    evenSum += reversed[i];
+  }
+  var finalSum = oddSum + evenSum;
+  if(finalSum % 10 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+module.exports = check;
--- modulus10/test/test.js
+++ modulus10/test/test.js
@@ -4,12 +4,12 @@ var checkNumber = require("../lib/check");
 describe("Check Number", function() {
   it("Should return true if a number is valid", function(){
     var finalNum = 49927398716;
-    var result = check(finalNum);
+    var result = checkNumber(finalNum);
     assert.isTrue(result);
   });
   it("Should return false if a number is not valid", function(){
     var finalNum = 49927398717;
-    var result = check(finalNum);
+    var result = checkNumber(finalNum);
     assert.isFalse(result);
   });
 });
